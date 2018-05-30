2b99b1e06417d18286ff1535fa01e5799940197c
sonofeft <ctatsourceforge@gmail.com>
2018-05-30 08:42:21 -0700

    try apt-get in travis build

--- rocketcea/sphinx_html/_static/bizstyle.js
+++ rocketcea/sphinx_html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("RocketCEA 0.1.5 documentation");
+        $("li.nav-item-0 a").text("RocketCEA 1.0.1 documentation");
     }
 });
