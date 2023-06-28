b83568597b3bfd651521d85c01ed669e8ae21bbd
Charlie Taylor <sonofeft@users.noreply.github.com>
2023-06-28 14:22:47 -0700

    some build cleanup

--- build_src_dist.bat
+++ build_src_dist.bat
@@ -1,7 +1,7 @@
 
 
 cd .\docs
-D:\python37_64\Scripts\sphinx-build.exe -b html -d _build/doctrees  . _build/html
+C:\Python310\Scripts\sphinx-build.exe -b html -d _build/doctrees  . _build/html
 cd ..
 
 xcopy /S .\docs\_build\html .\rocketcea\sphinx_html /y
