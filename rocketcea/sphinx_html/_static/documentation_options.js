f6af6f2d1c755c351679efa0ce5e2ab063ff95af
Charlie Taylor <sonofeft@users.noreply.github.com>
2023-06-28 14:37:14 -0700

    rebuilt docs

--- rocketcea/sphinx_html/_static/documentation_options.js
+++ rocketcea/sphinx_html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.1.30',
+    VERSION: 'v1.1.31',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
