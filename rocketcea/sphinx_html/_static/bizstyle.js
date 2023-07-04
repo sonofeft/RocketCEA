9d9deda653a30642e273f67d46970ce483135fd5
Charlie Taylor <sonofeft@users.noreply.github.com>
2023-07-04 13:34:44 -0700

    doc rebuild

--- rocketcea/sphinx_html/_static/bizstyle.js
+++ rocketcea/sphinx_html/_static/bizstyle.js
@@ -23,7 +23,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "RocketCEA v1.1.33 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "RocketCEA v1.1.34 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
