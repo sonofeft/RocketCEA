caca9415d6cbf2911c354adf899e72abf713eb14
Charlie Taylor <sonofeft@users.noreply.github.com>
2026-01-17 13:46:28 -0800

    updated wheel making code

---
+++ make_all_wheels.bat
@@ -0,0 +1,10 @@
+
+REM Create all supported python wheels 
+call run_venv_pytest.bat 7
+call run_venv_pytest.bat 8
+call run_venv_pytest.bat 9
+call run_venv_pytest.bat 10
+call run_venv_pytest.bat 11
+call run_venv_pytest.bat 12
+call run_venv_pytest.bat 13
+call run_venv_pytest.bat 14
--- run_venv_pytest.bat
+++ run_venv_pytest.bat
@@ -5,19 +5,19 @@ setlocal
 set SAVED_DIR=%CD%
 
 if "%1"=="" (
-    echo Usage: run_venv_pytest.bat 10
-    echo         run_venv_pytest.bat 12
+    echo ===> Usage: run_venv_pytest.bat 10
+    echo ===>         run_venv_pytest.bat 12
     exit /b 1
 )
 
 set PYVER=%1
 set VENV=venvs\py3%PYVER%
 
-echo === Using Python 3.%PYVER% ===
+echo ===>  Using Python 3.%PYVER% ===
 
 rem Create venv if missing
 if not exist "%VENV%" (
-    echo Creating venv for Python 3.%PYVER%...
+    echo ===> Creating venv for Python 3.%PYVER%...
     py -3.%PYVER% -m venv "%VENV%"
 )
 
@@ -32,10 +32,20 @@ pip install pytest
 
 rem Run pytest from Lib\site-packages\rocketcea
 pytest
+set TESTRESULT=%ERRORLEVEL%
+
+cd "%SAVED_DIR%"
+
+if "%TESTRESULT%"=="0" (
+    echo ===> Pytest passed. Building wheel...
+    python -m pip install --upgrade build
+    python -m build --wheel
+) else (
+    echo ===> Pytest failed. Wheel will NOT be built.
+)
 
 rem Deactivate
 call deactivate
 
-cd "%SAVED_DIR%"
 endlocal
 
